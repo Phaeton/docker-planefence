2535cab234bfc89e853ed1105854b882199aa3d3
Dziban303 <dziban303@users.noreply.github.com>
2023-11-30 14:17:35 -0600

    add freeform search for exclusions

--- rootfs/usr/share/plane-alert/get-pa-alertlist.sh
+++ rootfs/usr/share/plane-alert/get-pa-alertlist.sh
@@ -63,6 +63,10 @@ if [[ $inhibit_update == "false" ]]; then
 		then
 			echo "$TYPE appears to be an ICAO hex and is valid, removing."
 			sed -r -i "/^$TYPE,/Id" /usr/share/planefence/persist/.internal/plane-alert-db.txt
+		elif [[ -n "$TYPE" ]]
+		then
+			echo "$TYPE appears to be a freeform search pattern, I dearly hope you know what you're doing!"
+			sed -r -i "/,[A-Za-z0-9 ]*$TYPE[A-Za-z0-9 ]*,/Id /usr/share/planefence/persist/.internal/plane-alert-db.txt
 		else
 			echo "$TYPE is invalid, skipping!"
 		fi
